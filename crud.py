1b7ccc1bd602eecdcafa225fbdd6729127c234be
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-03-15 00:19:30 +0000

    Second commit, login and registration

--- crud.py
+++ crud.py
@@ -7,11 +7,31 @@ def create_user(fullname, email, password, address):
 
     return user
 
+def get_password_by_email(email):
+    """ Return a password by user email """
 
-def create_dog_profile(dog_name, dog_age, dog_size, dog_breed):
+    user = User.query.filter(User.email==email).first()
+    
+    return user.password
+
+def get_user_id_by_email(email):
+    """ Return user id by user email"""
+
+    user = User.query.filter(User.email==email).first()
+
+    return user.user_id
+
+def get_user_by_email(email):
+    """Return a user by their email """
+
+    user = User.query.filter(User.email==email).first()
+
+    return user
+
+def create_dog_profile(user_id, dog_name, dog_age, dog_size, dog_breed):
     """ Create and return a dog profile """
 
-    dog = Dog(dog_name=dog_name, dog_age=dog_age, dog_size=dog_size, dog_breed=dog_breed)
+    dog = Dog(user_id=user_id, dog_name=dog_name, dog_age=dog_age, dog_size=dog_size, dog_breed=dog_breed)
 
     return dog
 
--- server.py
+++ server.py
@@ -8,6 +8,64 @@ app = Flask(__name__)
 app.secret_key = "key"
 app.jinja_env.undefined = StrictUndefined
 
+
+#Creating homepage route!
+
+@app.route("/")
+def show_homepage():
+    """Show homepage"""
+
+    return render_template('homepage.html')
+
+@app.route('/login', methods = ['POST'])
+def login():
+    """ Login with email and password """
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user_password = crud.get_password_by_email(email)
+
+    if user_password == password:
+        session['user'] = crud.get_user_id_by_email(email)
+        flash('Logged in!')
+    else:
+        flash('Incorrect password, please try again!')
+
+    return redirect('/')
+
+@app.route('/new_users', methods = ['POST'])
+def register_user():
+    """ Create a new user """
+
+    fullname = request.form.get('fullname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    address = request.form.get('address')
+    dog_name = request.form.get('dog_name')
+    dog_age = request.form.get('dog_age')
+    dog_size = request.form.get('dog_size')
+    dog_breed = request.form.get('dog_breed')
+    
+    user = crud.get_user_by_email(email)
+    
+
+    if user:
+        flash('This email has already been used. Try a different email.')
+    else:
+        new_user = crud.create_user(fullname, email, password, address)
+        db.session.add(new_user)
+        db.session.commit()
+        # user_id = new_user.user_id
+        user = crud.get_user_id_by_email(email)
+        dog = crud.create_dog_profile(user, dog_name, dog_age, dog_size, dog_breed)
+        db.session.add(dog)
+        db.session.commit()
+        flash('Your account has been successfully created. You can log in now')
+
+    return redirect('/')
+
+
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
     connect_to_db(app)
